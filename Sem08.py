324bbd2d72193347dd080de2fa97a8fa2df69062
kelvincabezas <ruben.cabezas@outlook.com>
2024-08-16 20:30:39 -0500

    leer archivo

--- Sem08.py
+++ Sem08.py
@@ -6,3 +6,11 @@ st.title("K-Means Clustering con Streamlit")
 
 #subir archivo excel
 uploaded_file = st.file_uploader("subir un archivo excel",type=["xlsx"])
+
+if uploaded_file isnotNone:
+    #Leer archivo
+    pd=pd.read_excel(uploaded_file)
+
+    st.write("### vista previa de los datos")
+    st.write(df.head())
+    
